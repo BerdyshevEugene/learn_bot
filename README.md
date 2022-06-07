aa697d0bf26e8a8a910e636bfa75f1d6c7c2c147
BerdyshevEugene <berdyshevevg@mail.ru>
2022-06-07 23:19:48 +0800

    1-st version

---
+++ README.md
@@ -0,0 +1,36 @@
+# Pocket translator
+##
+
+## Description
+
+- use this bot for quick translations from one language to another
+
+
+## Stack
+- Python 3.7
+- python-telegram-bot 13.7
+- translate 3.6.1.
+
+### Requirements
+- Клонировать репозиторий и перейти в него в командной строке
+```
+git clone https://github.com/BerdyshevEugene/api_final_yatube.git
+```
+```
+cd api_final_yatube
+```
+- Create and activate a virtual environment:
+
+```
+python3 -m venv env
+```
+```
+source env/bin/activate
+```
+```
+python3 -m pip install --upgrade pip
+```
+- Install dependencies from a file requirements.txt
+```
+pip install -r requirements.txt
+``` 
--- bot.py
+++ bot.py
@@ -1,33 +1,65 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-import settings 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          CallbackContext, CallbackQueryHandler)
+from translate import Translator
 
-logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+language = ""
+
+def select_language(update: Update, context: CallbackContext) -> None:
+    keyboard = [
+        [
+            InlineKeyboardButton('English', callback_data='English'),
+            InlineKeyboardButton('Spanish', callback_data='Spanish'),
+            InlineKeyboardButton('Russian', callback_data='Russian'),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Choose language!', reply_markup=reply_markup)
 
 
-PROXY = {'proxy_url': settings.PROXY_URL,
-    'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
+def button(update: Update, context: CallbackContext) -> None:
+    global language
+    language = update.callback_query.data.lower()
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text=f'{query.data} has been selected for'
+                            'translation. Enter your text.')
 
-def greet_user(update, context):
-    print('вызван /start')
-    update.message.reply_text('Здравствуй, пользователь')
 
-def talk_to_me(update, context):
-    text = update.message.text
-    print(text)
-    update.message.reply_text(text)
+def language_translator(user_input):
+    translator = Translator(from_lang="russian", to_lang=language)
+    translation = translator.translate(user_input)
+    return translation
+
+
+def reply(update, context):
+    user_input = update.message.text
+    update.message.reply_text(language_translator(user_input))
+
 
 def main():
-    mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
+    api = open("api.txt", "r")
+    updater = Updater(api.read(), use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('start', select_language))
+    dp.add_handler(CommandHandler('select_language', select_language))
+    dp.add_handler(CallbackQueryHandler(button))
+    dp.add_handler(MessageHandler(Filters.text, reply))
+    updater.start_polling()
+    updater.idle()
 
-    dp=mybot.dispatcher
-    dp.add_handler(CommandHandler('start', greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
-    logging.info('бот стартовал')
-    mybot.start_polling()
-    mybot.idle()
+if __name__ == '__main__':
 
-if __name__ == "__main__":
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        filename='main.log',
+        filemode='w',
+        level=logging.DEBUG
+    )
+ 
     main()
